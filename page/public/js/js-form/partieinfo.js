b7c213bd9827aab05d61f61ff9e5edad0bac1c37
tahaettabti <tahaettabti1234@gmail.com>
2020-04-06 01:38:54 +0100

    page

--- page/public/js/js-form/partieinfo.js
+++
@@ -1,8 +0,0 @@
-function myfunction() {
-    var x = document.getElementById("partie-info");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
